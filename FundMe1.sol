d6e0616491b9415f38d2a7216b53f1c475361c47
Saeed Aghamohammadi <saeed008190@gmail.com>
2023-08-09 17:14:47 +0330

    adding some new features to fund me contract

--- FundMe1.sol
+++ FundMe1.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 contract Fundme {
    
-   uint256 public minimumusd=5;
+   uint256 public minimumusd=5e18;
 
     // be used to call to send money to our contract
     // payable keyword makes the function to look red in the remix ui
@@ -14,14 +16,42 @@ contract Fundme {
        //msg.value is in eth and minimumusd is USD for convert the amount of ethereum to its price in dollar we use chainlink and oracle
        // to get the ethereum price in dollar we must use oracle network
        
-        require(msg.value>=minimumusd,"didn't send enough ETH"); //1e18 means 1 ETH= 100000000000000000000 wei
+        require(getConversionRate(msg.value)>=minimumusd,"didn't send enough ETH"); //1e18 means 1 ETH= 100000000000000000000 wei
         // in the above message, it says that is the first part is false then go ahead and revert the second section.
         
+    }   
+
+    function getPrice() public view returns(uint256) {
+        //Address 0x1a81afB8146aeFfCFc5E50e8479e826E7D55b910
+        //ABI 
+        AggregatorV3Interface priceFeed=AggregatorV3Interface(0x1a81afB8146aeFfCFc5E50e8479e826E7D55b910);
+        //(uint80 roundId, int256 price, uint256 startedAt, uint256 timestamp, uint80 answerdInRound)=priceFeed.latestRoundData();
+        //the line above return whole bunch of data but we just care about price so we rewrite it like:
+        (,int256 price,,,)=priceFeed.latestRoundData();
+
+        //price of ETH in terms of USD 
+        //price feed(int256) and msg.value(uint256) have different decimal places and solidity doesn't work with decimals
+        //for match up: (between int and uint we can use typecasting
+
+        return uint256(price *1e10);
+
     }
 
+    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+        uint256 ethPrice = getPrice();
+
+        uint256 ethAmountTnUsd= (ethPrice * ethAmount)/1e18;
+        return ethAmountTnUsd;
+
+    }
+
+    function getVersion() public  view returns(uint256) {
+        return AggregatorV3Interface(0x1a81afB8146aeFfCFc5E50e8479e826E7D55b910).version();
+    }
+    
 
     // be used to withdraw the money which is sent to us
-    function withdraw() public {}
+    //function withdraw() public {}
 
 
 
