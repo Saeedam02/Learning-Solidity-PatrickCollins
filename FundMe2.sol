944315acf92efda4a52a7c3a748cfb5362a5c92f
Saeed Aghamohammadi <saeed008190@gmail.com>
2023-08-10 13:57:00 +0330

    adding the for loop for the contract

--- FundMe2.sol
+++ FundMe2.sol
@@ -12,9 +12,21 @@ contract FundMe {
         require(msg.value.getConversionRate() >=minimumusd,"didn't send enough ETH"); 
 
         funders.push(msg.sender); // this refers to whoever call this function
-        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+        //addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;   it's better to show in the following way:
+        addressToAmountFunded[msg.sender] += msg.value;
 
     }
+    function withdraw() public {
+        //for loop
+        // [1,2,3,4] elements
+        //for(/*starting index,ending index, step amount */)
+        //funderIndex=funderIndex +1 is equal with funder++
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex=funderIndex +1) {
+            address funder= funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
 
+        }
+
+    }
 
 }
