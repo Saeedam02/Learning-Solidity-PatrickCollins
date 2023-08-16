4af054380318b577c1c484674e0c779a2cc38829
Saeed Aghamohammadi <saeed008190@gmail.com>
2023-08-16 15:28:24 +0330

    adding new test functiom to check that are the msg.sender and owner equal:

--- foundry-f23/foundry-fund-me-f23/test/FundMeTest.t.sol
+++ foundry-f23/foundry-fund-me-f23/test/FundMeTest.t.sol
@@ -8,6 +8,9 @@ contract FundMeTest is Test {
     FundMe fundMe; // making it as a stage variable till we can use it in the second function.abi
 
     function setUp() external {
+        // we are calling the FundMeTest which then deploys FundMe
+        // us -> FundMeTest -> FundMe
+        //so the owner of the FundMe is FundMeTest not us
         fundMe = new FundMe();
     }
 
@@ -15,6 +18,27 @@ contract FundMeTest is Test {
         assertEq(fundMe.MINIMUM_USD(), 5e18); // with this line we check that is the MINIMUM_USD equal to 5e18 or not.
         //if we test our program, we see that it'll pass but if we change 5e18 to 6e18 we see that it will fail.abi
     }
+
+    // the following function will fail.
+    // function testOwnerIsMsgSender() public {
+    //     assertEq(fundMe.i_owner(), msg.sender);
+    // }
+    //for debugging that we use this funnction:
+    // function testOwnerIsMsgSender() public {
+    //     console.log(fundMe.i_owner());
+    //     console.log(msg.sender);
+    //     assertEq(fundMe.i_owner(), msg.sender);
+    // }
+
+    //the reason of failing:
+    // us -> FundMeTest -> FundMe
+    //so the owner of the FundMe is FundMeTest not us
+    // instead of msg.sender we must check the our address
+    function testOwnerIsMsgSender() public {
+        assertEq(fundMe.i_owner(), address(this));
+    }
+
+    //this codes are related to getting familiar with a test function.
     // uint256 number = 1;
 
     // function setUp() external {
